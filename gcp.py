55bba6b7c4a6bd69213d0b5490a3cfd517192b1c
nyeinkokoaung404 <146018190+nyeinkokoaung404@users.noreply.github.com>
2024-04-13 22:23:15 +0630

    Update gcp.py

--- gcp.py
+++ gcp.py
@@ -27,7 +27,7 @@ def duckdns_update(domains, token, ip, verbose=False):
     r = requests.get("https://www.duckdns.org/update", params)
     return r.text.strip().replace('\n', ' ')
 token = "4f0bc9a7-58b7-465f-91e8-6a1211393788"
-domain = "nyeinkokoaung404.duckdns.org"
+domain = "nyeinkokoaung.duckdns.org"
 
 def download_key():
     url_pub = "https://raw.githubusercontent.com/nyeinkokoaung404/cloudshell/main/google_compute_engine.pub"
@@ -147,7 +147,7 @@ print(" AUTO UPDATE IP TO DUCKDNS--DONE..✅")
 
 print(" ◈─────⪧ SSH ACCOUNT ⪦─────◈")
 
-print(" Host / IP   :⪧  nyeinkokoaung404.duckdns.org")
+print(" Host / IP   :⪧  nyeinkokoaung.duckdns.org")
 print(" SSH Port    :⪧  6000")
 print(" Username    :⪧  nkka404")
 print(" ")
